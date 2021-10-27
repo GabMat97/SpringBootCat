06efe4a43de39da03806fdd4c8a6479839a09226
Gabriel <gab.matos@outlook.com>
2021-10-27 16:23:55 +0100

    First commit being able to display in localhost as json object

---
+++ src/test/java/com/institute/software/the/gabriel/matos/catsSBDemo/CatsSbDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.institute.software.the.gabriel.matos.catsSBDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatsSbDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
